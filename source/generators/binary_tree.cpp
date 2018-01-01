39d3348b50095882c95262192f7e687ce5fc7da9
Jakob <jakob.ruhl@gmail.com>
2018-01-01 18:24:42 +0100

    Added binary tree algorithm

---
+++ source/generators/binary_tree.cpp
@@ -0,0 +1,33 @@
+#include "binary_tree.h"
+
+#include "../math/probability.h"
+#include "../core/grid.h"
+#include "../core/cell.h"
+
+namespace maze
+{
+    namespace binary_tree
+    {
+        void apply(grid* grid)
+        {
+            for (unsigned row = 0; row < grid->number_of_rows(); ++row)
+            {
+                for (unsigned collumn = 0; collumn < grid->number_of_collumns(); ++collumn)
+                {
+                    bool link_north = math::probability::flip_coin();
+                    bool at_north_border = row == 0;
+                    bool at_east_border = collumn == grid->number_of_collumns() - 1;
+
+                    if (at_east_border || (!at_north_border && link_north))
+                    {
+                        grid->get_cell(row, collumn)->link_with(dir::NORTH);
+                    }
+                    else
+                    {
+                        grid->get_cell(row, collumn)->link_with(dir::EAST);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ source/generators/binary_tree.h
@@ -0,0 +1,14 @@
+
+//forward decleration
+namespace maze
+{
+    class grid;
+}
+
+namespace maze
+{
+    namespace binary_tree
+    {
+        void apply(grid* grid);
+    }
+}
